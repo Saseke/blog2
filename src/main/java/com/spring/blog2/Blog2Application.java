85561e277ff3ef3c953e1c81741f61ba777b6cd5
Yoke-Yoke <717964921@qq.com>
2017-11-27 10:28:47 +0800

    first commit

---
+++ src/main/java/com/spring/blog2/Blog2Application.java
@@ -0,0 +1,12 @@
+package com.spring.blog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog2Application.class, args);
+	}
+}
